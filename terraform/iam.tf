cc263eb6c1e015b4d8127fd5d0eef60f0cb8df05
dhananjayvegito <80611712+dhananjayvegito@users.noreply.github.com>
2025-02-07 14:45:27 +0530

    Create iam.tf

---
+++ terraform/iam.tf
@@ -0,0 +1,14 @@
+# iam.tf
+resource "aws_iam_role" "eks_role" {
+  name = "eks-cluster-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [{
+    "Effect": "Allow",
+    "Principal": { "Service": "eks.amazonaws.com" },
+    "Action": "sts:AssumeRole"
+  }]
+}
+EOF
+}
